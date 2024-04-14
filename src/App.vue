a8fc291f02cc14906df9a3bc1ebfa6ee86b4e66e
Abduhamid <Abdukhamid000@gmail.com>
2024-04-14 13:14:20 +0500

    feat: first

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <v-btn variant="tonal"> Button </v-btn>
+    <v-card> GO GET IT </v-card>
+    <v-chip> Chip </v-chip>
+    <v-file-input label="File input"></v-file-input>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
